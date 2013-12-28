367191829dcd4411da554252d9415a848e9f1288
Jacky Lee <JackyLeeHongJian@Gmail.com>
2013-12-28 14:34:14 +0800

    Used cerr in case of error

--- multiDeviceAsync/multiDevice.cpp
+++ multiDeviceAsync/multiDevice.cpp
@@ -406,7 +406,7 @@ int main(int argc, char* argv[])
 					const float expected = lig.ligh[i] * 2.0f + 1.0f + prmh[i % 16];
 					if (fabs(actual - expected) > 1e-7)
 					{
-						cout << "cnfh[" << i << "] = " << actual << ", expected = " << expected << endl;
+						cerr << "cnfh[" << i << "] = " << actual << ", expected = " << expected << endl;
 						break;
 					}
 				}
